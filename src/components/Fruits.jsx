a739991a64df907234ccc410da77849398e80a6f
harshitanchan <harshitanchan647@gmail.com>
2024-08-04 22:58:12 +0530

    Rendering Array Of Objects

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,10 +1,31 @@
 export default function Fruits() {
-  const fruits = ["pair", "Banana", "Orange", "Apple"];
+  // const fruits = ["pair", "Banana", "Orange", "Apple"];
+
+  const fruits = [
+    {
+      name: "Apple",
+      price: 10,
+      emoji: "🍎",
+    },
+    {
+      name: "Mango",
+      price: 14,
+      emoji: "🥭",
+    },
+    {
+      name: "banana",
+      price: 19,
+      emoji: "🍌",
+    },
+  ];
   return (
     <div>
       <ul>
         {fruits.map((fruit) => (
-          <li key={fruit}> {fruit}</li>
+          <li key={fruit.name}>
+            {" "}
+            {fruit.emoji} {fruit.name} ${fruit.price}
+          </li>
         ))}
       </ul>
     </div>
