1f94dbf394de9bd2471d5a2239ada62685464fb7
harshitanchan <harshitanchan647@gmail.com>
2024-08-06 14:27:30 +0530

    This is a new commit for what I originally planned to be amended

--- src/components/Conditional.jsx
+++
@@ -1,7 +0,0 @@
-import Code from "./Code";
-import Welcome from "./Welcome";
-
-export default function ConditionalComponent() {
-  const display = true;
-  return display ? <Code /> : <Welcome />;
-}
--- todoapp/src/main.jsx
+++ todoapp/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Hello from "./components/Hello";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
